30c76768ad188a28f6435dc2287a8517b484ad57
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-03-12 20:51:26 +0530

    add static file serving and homepage route; create initial index.html

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const express = require('express');
 const app = express();
 const server = http.createServer(app);
 
-server.listen(9000, () => {`Server is listening to port ${9000}`})
+app.use(express.static('/public'));
+
+app.get('/', (req, res) => {
+    return res.sendFile('./public/index.html', { root: __dirname });
+})
+
+server.listen(9000, () => console.log(`Server is listening to port 9000`))
