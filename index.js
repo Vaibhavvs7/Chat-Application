2f0264abb4c643f7340fb4535be23be6faf8bf90
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-03-14 10:49:02 +0530

    implement real-time messaging with Socket.io; update index.html for chat interface

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require('http');
 const express = require('express');
+const {Server} = require('socket.io');
+const { Socket } = require('dgram');
+
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+
+//Socket.io
+io.on("connection", (socket) => {
+    socket.on('user-message', (message) => {
+        io.emit('message', message);
+    })
+});
 
 app.use(express.static('/public'));
 
